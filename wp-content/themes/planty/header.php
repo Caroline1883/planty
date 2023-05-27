65716f20029b998e13717136a57f9a7e2385187a
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-05-28 01:14:51 +0200

    Création CSS + Header thème enfant

---
+++ wp-content/themes/planty/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta name="viewport" content="width=device-width" />
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        <?php wp_body_open(); ?>
+        <div id="wrapper" class="hfeed">
+            <header id="header" role="banner">
+                <div id="branding">
+                    <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+                        <a href="index.php"><img class="logo" src="http://localhost/planty/wp-content/uploads/2023/05/Logo.png" alt="Logo Planty" id="logo"></a>
+                    </div>
+                    <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+                        <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<div class="menu-item" itemprop="name">', 'link_after' => '</div>' ) ); ?>
+                        <!-- <div class="menu-item">
+                            <a href="#">Nous rencontrer</a>
+                            <a href="#">Admin</a>
+                        </div>
+                        <a class="menu-item-commander" href="#">Commander</a> -->
+                    </nav>
+            </header>
+        <div id="container">
+            <main id="content" role="main">
