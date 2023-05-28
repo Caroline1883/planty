099fdb5dcc6c7bfa70459b072544fd6569066c81
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-05-28 23:07:37 +0200

    Ajout footer

---
+++ wp-content/themes/planty/footer.php
@@ -0,0 +1,12 @@
+</main>
+    <?php get_sidebar(); ?>
+    </div>
+        <footer id="footer" class="footer" role="contentinfo">
+            <div id="copyright">
+                <a href="#" alt="Mentions">Mentions légales</a>
+            </div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
--- wp-content/themes/planty/header.php
+++ wp-content/themes/planty/header.php
@@ -13,13 +13,8 @@
                     <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
                         <a href="index.php"><img class="logo" src="http://localhost/planty/wp-content/uploads/2023/05/Logo.png" alt="Logo Planty" id="logo"></a>
                     </div>
-                    <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+                    <nav id="menu" class="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
                         <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<div class="menu-item" itemprop="name">', 'link_after' => '</div>' ) ); ?>
-                        <!-- <div class="menu-item">
-                            <a href="#">Nous rencontrer</a>
-                            <a href="#">Admin</a>
-                        </div>
-                        <a class="menu-item-commander" href="#">Commander</a> -->
                     </nav>
             </header>
         <div id="container">
