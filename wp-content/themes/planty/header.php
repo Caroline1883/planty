cf1360f8ab3a164908c115c2cc99d20b182b44ff
Caroline1883 <36411250+Caroline1883@users.noreply.github.com>
2023-05-30 00:09:18 +0200

    Modification header (responsive) + footer

--- wp-content/themes/planty/header.php
+++ wp-content/themes/planty/header.php
@@ -13,12 +13,17 @@
                     <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
                         <a href="index.php"><img class="logo" src="http://localhost/planty/wp-content/uploads/2023/05/Logo.png" alt="Logo Planty" id="logo"></a>
                     </div>
-                    <nav id="menu" class="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+                </div>
+                <nav id="menu" class="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+                    <label for="toggle">☰</label>
+                    <input type="checkbox" id="toggle">
+                    <div class="menu_responsive">
                         <?php wp_nav_menu( 
                             array( 'theme_location' => 'main-menu', 
-                            'link_before' => '<div itemprop="name">', 
-                            'link_after' => '</div>' ) ); ?>
-                    </nav>
+                           'link_before' => '<div itemprop="name">', 
+                           'link_after' => '</div>' ) ); ?>
+                    </div>
+                </nav>
             </header>
         <div id="container">
             <main id="content" role="main">
